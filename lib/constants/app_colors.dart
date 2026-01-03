b66691188a0215a767d69add0ad8b97bbc09df56
abebaw8760 <abebawabebe841@gmail.com>
2026-01-03 02:34:58 -0800

    Update app_colors.dart

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -1,3 +1,8 @@
 import 'package:flutter/material.dart';
 
-// do ur task here
+class AppColors {
+  static const Color primary = Color(0xFFFF5A7E);
+  static const Color background = Color(0xFFF8F8F8);
+  static const Color textDark = Color(0xFF1C1C1C);
+  static const Color textLight = Color(0xFF9B9B9B);
+}
